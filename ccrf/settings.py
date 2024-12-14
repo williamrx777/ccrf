37c1251a0d4b52becd1cdaedd4e8b54d38739347
William Rodrigues <williamr723@gmail.com>
2024-12-14 17:37:14 -0300

    `Update settings.py: set DEBUG to False and ALLOWED_HOSTS to '*'`

--- ccrf/settings.py
+++ ccrf/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6vtup(s_&yeimh1e=1i%lonq$za9rq350leonoy^da4%mf$g2n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
