db2a8eaf9d68a84b0967155cfba41de2de2eb77b
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-01-20 16:10:03 -0300

    fix import os

--- ccrf/settings.py
+++ ccrf/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 # Import dj-database-url at the beginning of the file.
 import dj_database_url
-from pathlib import Path, os
+from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
